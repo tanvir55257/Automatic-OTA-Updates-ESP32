74b2432f1076b869931b3a3f826927f484068cfd
MD Tanvir Shakil <82879004+tanvir55257@users.noreply.github.com>
2024-01-14 13:21:53 +0600

    Update ota.php

--- ota.php
+++ ota.php
@@ -1,11 +1,3 @@
-/************************************************************
-  @ Module     : ESP32 OTA basic
-                 (checking time of the file in server) source.
-  @ Language   : PHP
-by MD tanvir shakil
-************************************************************/
-
-
 <?php
 $filename = 'YOUR_BINARY_FILE';
 if (file_exists($filename)) {
